f80b80228920edbcae89e7ca30e26d70145fa922
Matthew <mh1753@york.ac.uk>
2018-11-08 11:07:50 +0000

    This hardly did anything but here it is

--- hangover/core/src/com/hangover/AKillerHangover.java
+++ hangover/core/src/com/hangover/AKillerHangover.java
@@ -1,12 +1,12 @@
 package com.hangover;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class AKillerHangover extends ApplicationAdapter {
+public class AKillerHangover extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
