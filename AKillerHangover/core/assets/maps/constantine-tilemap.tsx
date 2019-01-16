4f18650569e33216bd2fe365d4db8e568f30ebf6
Unknown <ep990@york.ac.uk>
2019-01-16 20:40:58 +0000

    Adding Constantine and Langwith map files

---
+++ AKillerHangover/core/assets/maps/constantine-tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="constantine-tiles" tilewidth="32" tileheight="32" tilecount="90" columns="15">
+ <image source="constantine-tiles.png" width="480" height="192"/>
+</tileset>
