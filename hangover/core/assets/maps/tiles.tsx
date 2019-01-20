065b6ebf1563658fc112a2a7ddd094d9fea8c15c
Unknown <mh1753@york.ac.uk>
2019-01-20 16:09:26 +0000

    Removed old version of the game

--- hangover/core/assets/maps/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="tiles" tilewidth="32" tileheight="32" tilecount="28" columns="7">
- <image source="tiles.png" trans="ffffff" width="160" height="192"/>
-</tileset>
